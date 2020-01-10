3a82caab57a5806d2f3174bbc4dfb004ff956f8e
Vladimir Kovacevic <vladimir.kovacevic@sbgenomics.com>
2020-01-10 12:18:06 +0100

    cuda 10.1.

---
+++ build_all.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+cd base-notebook/
+docker build -t images.sbgenomics.com/vladimirk/base-notebook-cuda:10.1 .
+cd ../minimal-notebook
+docker build -t images.sbgenomics.com/vladimirk/minimal-notebook-cuda:10.1 .
+cd ../scipy-notebook
+docker build -t images.sbgenomics.com/vladimirk/scipy-notebook-cuda:10.1 .
+cd ../tensorflow-notebook
+docker build -t images.sbgenomics.com/vladimirk/tensorflow-notebook-cuda:10.1 .
+docker push  images.sbgenomics.com/vladimirk/tensorflow-notebook-cuda:10.1
+
